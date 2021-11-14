8dab862f8b1858b5ed76052e23310b6e2bc017ca
Ma7Moo <88137754+Ma7Moo@users.noreply.github.com>
2021-11-14 11:00:29 +0100

    Update function.py

--- su15myju/function.py
+++ su15myju/function.py
@@ -6,25 +6,19 @@
 
 import numpy as np
 import matplotlib as plt
+from ipywidgets import interact, fixed
 from PIL import Image
 
+"""
+You should create a way to resize an image from an array X.
+The use of widgets is optional but you can take a look to interact.
+We should be able to install this package in Google Colab from your Git
+repo.
 
+"""
+# there wasn't a specific approach given, for this reason, I used this one where the user can input his/her desired width, hieght.
 
-
-#X.shape[0]
-resized = np.reshape(X,(X.shape[0],X.shape[1]))
-im = Image.fromarray(resized)
-im.show()
-
-
-# In[7]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+def imshow(X,width,height):
+    ImFromArr = Image.fromarray(X)
+    resizeImage = ImFromArr.resize((width,height))
+    resizeImage.show()
